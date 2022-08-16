ef5a85cb9bc8c0655ecb3cecbb06bd669a8b3a9d
Tomas <gonzalez.tommy2001@gmail.com>
2022-08-15 20:46:00 -0400

    Watcher now only checking for markdown files

--- src/utils/watcher.ts
+++ src/utils/watcher.ts
@@ -3,33 +3,21 @@ import Console from "./console";
 import * as debounce from "debounce";
 import { IConfigManager } from "../models/config-manager";
 
-export default (
-  path: string,
-  configManager: IConfigManager,
-  reloadSocket: () => void
-) =>
+export default (path: string, reloadSocket: () => void) =>
   new Promise((resolve /* , reject */) => {
     Console.debug("PATH:", path);
     const watcher = chokidar.watch(path, {
       // TODO: This watcher is not ready yet
+      ignored: /^(?=.*(\.\w+)$)(?!.*\.md$).*$/gm,
       ignoreInitial: true,
     });
     const onChange = (eventname: string, _filename: string) => {
       resolve(eventname /* , filename */);
-      Console.log(_filename);
-      if (
-        /^(?=.*(\\\w+)$).*$/gm.test(_filename) === false ||
-        /^(?=.*(\.\w+)$)(?!.*\.md$).*$/gm.test(_filename) === false
-      ) {
-        configManager.buildIndex();
-      } else {
-        reloadSocket();
-      }
+      reloadSocket();
     };
 
     watcher.on("all", debounce(onChange, 500, true));
     // watcher.on('all', onChange)
-
     process.on("SIGINT", function () {
       watcher.close();
       process.exit();
